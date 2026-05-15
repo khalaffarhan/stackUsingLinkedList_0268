1b7b7421b209a31ba19b13293b8ed15f304d7107
khalaffarhan <khalaffarhan54@gmail.com>
2026-05-15 15:49:54 +0700

    commit 14

--- stack.cpp
+++ stack.cpp
@@ -65,4 +65,12 @@ public:
         {
             Node *current = top;
             while (current != NULL)
-            
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
+    }
+
+   
