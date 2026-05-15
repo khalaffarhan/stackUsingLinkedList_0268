3fd55bbd82ad680ae97f55d6c1e5f012169e83a6
khalaffarhan <khalaffarhan54@gmail.com>
2026-05-15 15:52:17 +0700

    commit 18

--- stack.cpp
+++ stack.cpp
@@ -95,4 +95,14 @@ int main()
         cout << "Enter your choice: ";
         cin >> choice;
 
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value);
+            break;
+        case 2:
+            stack.pop();
+            break;
         
