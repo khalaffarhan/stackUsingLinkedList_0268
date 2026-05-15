ce2b2501752087edae9af6319faff92be4f3ea54
khalaffarhan <khalaffarhan54@gmail.com>
2026-05-15 15:20:05 +0700

    commit 8

--- stack.cpp
+++ stack.cpp
@@ -35,4 +35,10 @@ public:
     {
         Node *newNode = new Node();
         newNode->data = value;
-       
+        newNode->next = top;
+        top = newNode;
+        cout << "push value: " << value << endl;
+        return value;
+    }
+
+    
